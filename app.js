1e9aa0d5131a2d8977c62da314c1543d2884656f
L Barbee <bluelb84@gmail.com>
2017-08-27 19:39:14 -0400

    testing API call endpoint

--- app.js
+++ app.js
@@ -18,13 +18,17 @@ const STATE = {
     method: 'pet.find'
 }
 
-function getDataFromAPI(method, callback) {
+function getDataFromAPI(callback) {
     STATE.method = method;
     let apiURL = STATE.petfinder_search_url + `${STATE.method}`;
-    $.getJSON(apiURL, STATE.query, callback);
+    // $.getJSON(apiURL, STATE.query, callback);
+    $.getJSON('http://api.petfinder.com/pet.find?format=json&key=ba13b6abb4f8162d2d70780f5d2a8d35&callback=?')
+    .done(function(petApiData) { alert('Data retrieved!')})
+    .error(function(err) { alert('Error retrieving data!'); 
+  });
 }
 
-console.log(getDataFromAPI('pet.find', displayPetfinderData));
+console.log(getDataFromAPI(displayPetfinderData));
 
 function displayPetfinderData(data) {
     STATE.data = data;
