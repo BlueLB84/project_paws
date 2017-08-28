464334706e7145b121ef1090579d95efefab88f9
L Barbee <bluelb84@gmail.com>
2017-08-27 20:57:46 -0400

    testing out API call

--- app.js
+++ app.js
@@ -3,36 +3,23 @@ const STATE = {
     query: {
         key: 'ba13b6abb4f8162d2d70780f5d2a8d35',
         animal: 'dog',
-        location: 'action, ma',
-        // breed1: null,
-        // breed2: null,
-        // sex1: null,
-        // sex2: null,
-        // size: null,
-        // age: null,
-        // id: null,
+        location: 'acton, ma',
+        callback: '?',
         output: 'basic',
         format: 'json',
     },
-    petfinder_search_url: 'http://api.petfinder.com/',
+    petfinder_search_url: 'https://api.petfinder.com/',
     method: 'pet.find'
 }
 
 function getDataFromAPI(callback) {
-    STATE.method = method;
     let apiURL = STATE.petfinder_search_url + `${STATE.method}`;
-    // $.getJSON(apiURL, STATE.query, callback);
-    $.getJSON('https://api.petfinder.com/pet.find?format=json&key=ba13b6abb4f8162d2d70780f5d2a8d35&callback=?')
-    .done(function(petApiData) { alert('Data retrieved!')})
-    .error(function(err) { alert('Error retrieving data!'); 
-  });
-}
-
-console.log(getDataFromAPI(displayPetfinderData));
+    $.getJSON(apiURL, STATE.query, callback).done(function(petApiData) {alert('Data retrieved!')});
+  };
 
 function displayPetfinderData(data) {
     STATE.data = data;
-    const results = data.pet.map((item, index) => {
+    const results = data.pets.map((item, index) => {
         return renderDataResults(item);
     })
     console.log(results);
@@ -41,3 +28,5 @@ function displayPetfinderData(data) {
 function renderDataResults(result) {
     return `${result.name}`
 }
+
+$(getDataFromAPI);
