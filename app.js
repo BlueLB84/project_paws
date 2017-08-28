bf4c744dadf7eed47534146444b16f675adacd10
L Barbee <bluelb84@gmail.com>
2017-08-28 08:47:05 -0400

    fixed API call issues

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const STATE = {
     data: null,
     query: {
-        api_key: 'DEMO_KEY',
-        // animal: 'dog',
-        // location: 'acton, ma',
-        // callback: '?',
-        // output: 'basic',
-        // format: 'json',
+        key: 'ba13b6abb4f8162d2d70780f5d2a8d35',
+        animal: 'dog',
+        location: 'acton,ma',
+        output: null,
+        format: 'json',
     },
-    petfinder_search_url: 'https://api.nasa.gov/planetary/apod?',
-    // method: 'pet.find'
+    petfinder_search_url: 'https://api.petfinder.com/',
+    method: 'pet.find'
 }
 
 function getDataFromAPI(callback) {
-    // let apiURL = STATE.petfinder_search_url + STATE.query.api_key;
-    // $.getJSON('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', callback);
-    $.getJSON('https://api.petfinder.com/pet.find?format=json&key=ba13b6abb4f8162d2d70780f5d2a8d35&location=acton,ma&callback=?', callback);
+    
+    let apiURL = STATE.petfinder_search_url + STATE.method + "?callback=?";
+    $.getJSON(apiURL, STATE.query, callback);
   };
+
+//   'https://api.petfinder.com/pet.find?format=json&key=ba13b6abb4f8162d2d70780f5d2a8d35&location=acton,ma&callback=?'
 
 function displayPetfinderData(data) {
     STATE.data = data;
