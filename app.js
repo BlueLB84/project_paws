f15409b45ad08976f166eaceef3e08e838c1ca09
L Barbee <bluelb84@gmail.com>
2017-09-04 21:03:22 -0400

    fixed image folder issue

--- app.js
+++ app.js
@@ -288,7 +288,7 @@ function renderPetResults(result) {
         return `<div class="result-dog">
         <h2>Sorry! There are no results <i class="fa fa-paw" aria-hidden="true"></i></h2>
         <figure>
-        <img src="images/chief_sleeping.JPG" alt="Chief sleeping from Instagram @chiefandzoe" />
+        <img src="/images/chief_sleeping.jpg" alt="Chief sleeping from Instagram @chiefandzoe" />
         <figcaption>A sleeping Chief. <i class="fa fa-instagram" aria-hidden="true"></i>&nbsp;<a href="https://www.instagram.com/chiefandzoe/" target="_blank" class="instagram"> @chiefandzoe</a></figcaption>
         </figure>
         </div>`
@@ -325,7 +325,7 @@ function renderPetResults(result) {
 function displayImages(result) {
     if(!result.media || !result.media.photos) {
         return `<figure>
-        <img src="images/zoe_no_image.jpg" alt="no image available" />
+        <img src="/images/zoe_no_image.jpg" alt="no image available" />
         <figcaption>Zoe <i class="fa fa-instagram" aria-hidden="true"></i>&nbsp;<a href="https://www.instagram.com/chiefandzoe/" target="_blank">@chiefandzoe class="instagram"</a> </figcaption>
         </figure>`
     } else {
